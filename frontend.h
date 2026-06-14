63efea97611aa87f615c9b1d4850b63ad532ad9a
eh-rud <dg060127@gmail.com>
2026-06-14 21:50:49 +0900

    Delete frontend.h

--- frontend.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "Types.h"
-#include "UI_Core.h"
-
-void ApplySmartFarmTheme();
-void RenderSmartFarmBakeryUI(const FactorySnapshot& snapshot, UIState& uiState, FactoryCommand& command);
